6568981ec5842281912a211edec0025372878a1b
Nadiia17 <nababiy@gmail.com>
2023-10-11 19:28:56 +0300

    searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,14 @@
 import { Component } from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
-import { ContactList } from './ContactList/ContactList';
-import { AppContainer, Subtitle, Title } from './App.styled';
+import { AppContainer } from './Layout';
+import { Searchbar } from './Searchbar/Searchbar';
 
 export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contact) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
-  };
-
-  handleAddContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
+  state = {};
 
   render() {
-    const { filter, contacts } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
     return (
       <AppContainer>
-        <Title>Phonebook</Title>
-        <ContactForm
-          onFormSubmit={this.handleAddContact}
-          currentContacts={contacts}
-        />
-        <Subtitle>Contacts</Subtitle>
-        <Filter value={filter} onChange={this.handleChange} />
-        <ContactList
-          contacts={filteredContacts}
-          onDelete={this.deleteContact}
-        />
+        <Searchbar onSubmit={values => console.log(values)} />
       </AppContainer>
     );
   }
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,47 @@
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import {
+  Button,
+  ButtonLabel,
+  SearchForm,
+  SearchFormInput,
+  ErrorMessageWrapper,
+  SearchbarWrapper,
+  IconWrapper,
+} from './Searchbar.styled';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const PhonebookSchema = Yup.object().shape({
+  searchQuery: Yup.string().min(2).required('Enter serach query'),
+});
+
+export const Searchbar = ({ onSubmit }) => (
+  <SearchbarWrapper>
+    <Formik
+      initialValues={{
+        searchQuery: '',
+      }}
+      validationSchema={PhonebookSchema}
+      onSubmit={(values, actions) => {
+        onSubmit(values);
+        actions.resetForm();
+      }}
+    >
+      <SearchForm>
+        <Button type="submit">
+          <IconWrapper>
+            <AiOutlineSearch />
+          </IconWrapper>
+          <ButtonLabel>Search</ButtonLabel>
+        </Button>
+
+        <SearchFormInput
+          type="text"
+          name="searchQuery"
+          placeholder="Search images and photos"
+        />
+        <ErrorMessage name="searchQuery" component={ErrorMessageWrapper} />
+      </SearchForm>
+    </Formik>
+  </SearchbarWrapper>
+);
