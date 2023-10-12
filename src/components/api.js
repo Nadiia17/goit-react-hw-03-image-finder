2fb573d606bef133da5e721720ed731f8478f24f
Nadiia17 <nababiy@gmail.com>
2023-10-12 14:14:32 +0300

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://pixabay.com/api';
+// axios.defaults.baseURL = 'https://pixabay.com/api';
 const KEY = '39989925-901ad6a53913725b2cdfb60c2';
+const API_URL = 'https://pixabay.com/api/';
 
 export const searchByQuery = async (query, page = 1) => {
   const params = {
@@ -16,7 +17,8 @@ export const searchByQuery = async (query, page = 1) => {
   // };
 
   try {
-    const response = await axios.get('', { params });
+    console.log('Base URL:', axios.defaults.baseURL);
+    const response = await axios.get(API_URL, { params });
     return response.data;
   } catch (error) {
     console.error('Error fetching data: ', error);
